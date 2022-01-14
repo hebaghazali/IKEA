f32dcc06713fc3e1bad9d0354a62649caea78786
EmanSalah95 <seman9918@gmail.com>
2022-01-14 18:30:20 +0200

    get Categories in menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,58 @@
-var menu= document.getElementById("main-menu");
-var offCanva=document.getElementById("offcanvasExample");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvz1el-WaDgUU36xlWeIEj2G9iyI0ewW4",
+  authDomain: "ikea-8dc72.firebaseapp.com",
+  projectId: "ikea-8dc72",
+  storageBucket: "ikea-8dc72.appspot.com",
+  messagingSenderId: "293717792182",
+  appId: "1:293717792182:web:f170e2edfe2370c9769d17",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const fireStore = getFirestore(app);
+
+var menu = document.getElementById("main-menu");
+var offCanva = document.getElementById("offcanvasExample");
 var content;
-function ShowDiv(id){
-    content = document.getElementById(id);
-    content.style.display="block";
-    menu.style.display="none";
+
+async function getCategories() {
+  let prCatListEl = document.getElementById("products-list");
+
+  prCatListEl.innerHTML = "";
+  var allCategories = await getDocs(collection(fireStore, "ProductCategories"));
+
+  allCategories.docs.forEach((element) => {
+    console.log(element.id, element.data().Name);
+    prCatListEl.innerHTML += 
+    `<li><a href="./pages/category.html" id=${element.id} onclick="setSession(this)">
+      ${element.data().Name} </a></li>`;
+  });
 }
-function ShowMain(){
-    menu.style.display="block";
-    content.style.display="none";
+
+window.setSession = setSession;
+function setSession(element) {
+    sessionStorage.setItem("subCategory",element.id)  
+}
+
+window.ShowDiv = ShowDiv;
+function ShowDiv(id) {
+  content = document.getElementById(id);
+  content.style.display = "block";
+  menu.style.display = "none";
+  if (id === "p-content") {
+    getCategories();
+  }
+}
+
+window.ShowMain = ShowMain;
+function ShowMain() {
+  menu.style.display = "block";
+  content.style.display = "none";
 }
