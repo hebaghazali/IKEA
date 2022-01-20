3a5c934e83e576f5d7afcb7435d0a40453d15571
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-01-20 21:14:18 +0200

    import css files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/main.scss';
+
 import App from './App';
 
 ReactDOM.render(
