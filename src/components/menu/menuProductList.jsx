b0ff722f7d12637599352f7c7476b619fa608602
raghda244 <raghdamohsen244@gmail.com>
2022-01-28 21:24:24 +0200

    update menu

--- src/components/menu/menuProductList.jsx
+++ src/components/menu/menuProductList.jsx
@@ -4,8 +4,8 @@ import {
   collection,
   getDocs,
 } from 'https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js';
-import { Link } from "react-router-dom";
-import { useState ,useEffect} from "react";
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBvz1el-WaDgUU36xlWeIEj2G9iyI0ewW4',
@@ -21,40 +21,46 @@ const app = initializeApp(firebaseConfig);
 const fireStore = getFirestore(app);
 
 const MenuProductList = (props) => {
-    const [categories, setCategories] = useState([]);
-    useEffect(()=>{
-        getDocs(collection(fireStore, 'ProductCategories')).then(
-            (allCategories) => {
-              setCategories(allCategories.docs);
-            }
-          );
-    },[])
-    return (
-        <>
-          <div className='offcanvas-body menu-body'>
-            <div className='d-flex flex-row'>
-              <button
-                className='position-sticky menu-btn'
-                onClick={() => {
-                  props.changeSelection('');
-                }}
-              >
-                <i className='bi bi-arrow-left-short'></i>
-              </button>
-              <span className='head-title'>Products</span>
-            </div>
-            <ul className='small-text-list my-4' id='products-list'>
-              {categories.map((category) => {
-                return (
-                  <li key={category.id}>
-                    <Link to={`category/product/${category.data().Name}/${category.id}`}>{category.data().Name}</Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </>
-    );      
-}
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getDocs(collection(fireStore, 'ProductCategories')).then(
+      (allCategories) => {
+        setCategories(allCategories.docs);
+      }
+    );
+  }, []);
+  return (
+    <>
+      <div className='offcanvas-body menu-body'>
+        <div className='d-flex flex-row'>
+          <button
+            className='position-sticky menu-btn'
+            onClick={() => {
+              props.changeSelection('');
+            }}
+          >
+            <i className='bi bi-arrow-left-short'></i>
+          </button>
+          <span className='head-title'>Products</span>
+        </div>
+        <ul className='small-text-list my-4' id='products-list'>
+          {categories.map((category) => {
+            return (
+              <li key={category.id}>
+                <Link
+                  to={`/category/product/${category.data().Name}/${
+                    category.id
+                  }`}
+                >
+                  {category.data().Name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default MenuProductList;
