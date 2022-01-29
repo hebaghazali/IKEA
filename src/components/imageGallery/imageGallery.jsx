5d06b22219e9500bb57d9d6923191de048f43885
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-01-29 20:31:54 +0200

    Rename 'class' to 'className'

--- src/components/imageGallery/imageGallery.jsx
+++ src/components/imageGallery/imageGallery.jsx
@@ -32,21 +32,21 @@ const ImageGallery = () => {
   ];
 
   return (
-    <div class='border-top mt-nav pt-nav pb-5'>
-      <div class='image-gallery row g-4'>
+    <div className='border-top mt-nav pt-nav pb-5'>
+      <div className='image-gallery row g-4'>
         {images.map(imageCol => {
           return (
             <>
               {images.indexOf(imageCol) === 0 ? (
-                <div class='col-12 col-lg-6'>
+                <div className='col-12 col-lg-6'>
                   <img src={imageCol} alt='sale' />
                 </div>
               ) : (
-                <div class='image-column col-6 col-lg-3'>
+                <div className='image-column col-6 col-lg-3'>
                   {imageCol.map(image => {
                     return (
                       <img
-                        class={`${image.size}-img`}
+                        className={`${image.size}-img`}
                         src={image.imageURL}
                         alt=''
                       />
