ebdac188153846f4c13460ee2eed45e471b62ca0
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-02-01 17:42:27 +0200

    Fix other conflicts

--- src/components/cards/productCard/productCard.jsx
+++ src/components/cards/productCard/productCard.jsx
@@ -49,7 +49,7 @@ const ProductCard = ({ showOptions, pId, productData }) => {
   };
 
   const addCart = () => {
-    dispatch(addToCart({ id: pId, productData, PurchasedAmount:1}));
+    dispatch(addToCart({ id: pId, productData, PurchasedAmount: 1 }));
     setInCart(true);
   };
 
@@ -71,25 +71,6 @@ const ProductCard = ({ showOptions, pId, productData }) => {
           </button>
         </header>
 
-<<<<<<< HEAD
-        <Link className='card category-card col-12 ' to={{
-          pathname: "/products/:pId",
-          state: {
-            prod:{ id: pId, productData } 
-          }
-        }}>
-          <img
-            // src='https://www.ikea.com/eg/en/images/products/soederhamn-chaise-longue-samsta-orange__0802365_pe768432_s5.jpg?f=xxs'
-            src={Images[isHovering ? 1 : 0]}
-            className='card-img-top'
-            alt={Name}
-            onMouseOver={() => setIsHovering(true)}
-            onMouseLeave={() => setIsHovering(false)}
-          />
-        </Link>
-
-=======
->>>>>>> hebaBranch
         <div className='mt-1 position-relative'>
           <Link
             className='card category-card col-12 '
