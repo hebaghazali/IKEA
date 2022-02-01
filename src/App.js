121b5e471540000a1204b345c7b624cdd5e807a1
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-02-01 19:05:16 +0200

    Change some styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Navbar from './components/navbar/navbar';
-import Home from './components/home.jsx';
+import Home from './pages/home.jsx';
 import Footer from './components/footer/footer';
 import Products from './pages/products/products';
 import Menu from './components/menu/menu';
@@ -20,15 +20,19 @@ function App() {
       <Menu />
       <div className='body-container'>
         <Navbar />
-        <div className='mt-nav-2 pt-nav border-top'>
+        <div className='mt-nav-4 pt-nav border-top'>
           <Switch>
             <Route path='/home' component={Home} />
-            <Route path='/category/:type/:name/:id' exact component={SubCategory} />
-            <Route path='/stores' component={StoresPage}/>
-            <Route path='/profile' component={Profile}/>
+            <Route
+              path='/category/:type/:name/:id'
+              exact
+              component={SubCategory}
+            />
+            <Route path='/stores' component={StoresPage} />
+            <Route path='/profile' component={Profile} />
             <Route path='/category/products' exact component={Products} />
             {/* <Route path='/category/:type/:name/:id/:subName/:subId' exact component={Products} /> */}
-            <Route path="/products/:pId" exact component={Testpage} />
+            <Route path='/products/:pId' exact component={Testpage} />
             <Redirect from='/' exact to='/home' />
           </Switch>
         </div>
