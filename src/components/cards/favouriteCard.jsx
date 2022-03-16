cb93aa98cacc440900b7688b7e245fcc960aa5f2
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-03-16 16:14:11 +0200

    Fix responsive issue

--- src/components/cards/favouriteCard.jsx
+++ src/components/cards/favouriteCard.jsx
@@ -9,11 +9,11 @@ import {
 import { useTranslation } from 'react-i18next';
 import { addToCart } from '../../store/actions/cartProducts';
 
-const FavouriteCard = (props) => {
-  const { t , i18n } = useTranslation();
+const FavouriteCard = props => {
+  const { t, i18n } = useTranslation();
   const [selectedAmount, setSelectedAmount] = useState(1);
 
-  const { cartProducts } = useSelector((state) => state.cartProducts);
+  const { cartProducts } = useSelector(state => state.cartProducts);
   let foundInCart = cartProducts?.find(i => i.id === props.id);
   const [inCart, setInCart] = useState(foundInCart ? true : false);
 
@@ -26,7 +26,7 @@ const FavouriteCard = (props) => {
     removeFavItemFromUser(localStorage.getItem('UID'), props.id);
   };
 
-  const selectAmount = (event) => {
+  const selectAmount = event => {
     setSelectedAmount(Number(event.target.value));
   };
 
@@ -52,48 +52,67 @@ const FavouriteCard = (props) => {
   return (
     <>
       <div className='shop-section'>
-        <div className='shop-icon'>
-          <button className='prod-box' onClick={() => deleteItem()}>
-            <i className='fas fa-trash-alt'></i>
-          </button>
+        <div className='shopping-list'>
+          <div className='shop-icon'>
+            <button className='prod-box' onClick={() => deleteItem()}>
+              <i className='fas fa-trash-alt'></i>
+            </button>
 
-          {props.product.Quantity!==0 &&
-          <div className='prod-box'>
-            <select defaultValue={selectedAmount} onChange={selectAmount}>
-              {(() => {
-                const options = [];
-                for (let i = 0; i < props.product.Quantity; i++) {
-                  options.push(<option key={i + 1}>{i + 1}</option>);
-                }
-                return options;
-              })()}
-            </select>
-          </div>}
-        </div>
+            {props.product.Quantity !== 0 && (
+              <div className='prod-box'>
+                <select defaultValue={selectedAmount} onChange={selectAmount}>
+                  {(() => {
+                    const options = [];
+                    for (let i = 0; i < props.product.Quantity; i++) {
+                      options.push(<option key={i + 1}>{i + 1}</option>);
+                    }
+                    return options;
+                  })()}
+                </select>
+              </div>
+            )}
+          </div>
 
-        <div className='shopping-img'>
-          <img src={props.product.Images[0]} alt='...' />
-        </div>
+          <div className='shopping-details'>
+            <div className='shopping-img'>
+              <img src={props.product.Images[0]} alt='...' />
+            </div>
 
-        <div className='shopping-info'>
-          <h6>{i18n.language=='en'?props.product.Name:props.product.NameAr}</h6>
-          <p>{i18n.language=='en'?props.product.Description:props.product.DescriptionAr}</p>
-          <h6>
-            {t('EGP')} {props.product.Price}
-          </h6>
-          <p className='txt-info'>
-            {i18n.language=='en'?props.product.Material:props.product.MaterialAr}, {props.product.Width}{t('cm')} * {props.product.Length}{t('cm')}
-          </p>
-          {/* <!-- button For Shopping --> */}
-          <div className='prod-box col-5'>
-            {props.product.Quantity!==0 &&
-            <button
-              className={`btn card-button ${inCart && 'disabled'}`}
-              onClick={addCart}
-            >
-              <i className='fas fa-shopping-bag'></i>{' '}
-              {!inCart ? t('AddToCart') : t('Added')}
-            </button>}
+            <div className='shopping-info'>
+              <h6>
+                {i18n.language == 'en'
+                  ? props.product.Name
+                  : props.product.NameAr}
+              </h6>
+              <p>
+                {i18n.language == 'en'
+                  ? props.product.Description
+                  : props.product.DescriptionAr}
+              </p>
+              <h6>
+                {t('EGP')} {props.product.Price}
+              </h6>
+              <p className='txt-info'>
+                {i18n.language == 'en'
+                  ? props.product.Material
+                  : props.product.MaterialAr}
+                , {props.product.Width}
+                {t('cm')} * {props.product.Length}
+                {t('cm')}
+              </p>
+              {/* <!-- button For Shopping --> */}
+              <div className='prod-box col-5'>
+                {props.product.Quantity !== 0 && (
+                  <button
+                    className={`btn card-button ${inCart && 'disabled'}`}
+                    onClick={addCart}
+                  >
+                    <i className='fas fa-shopping-bag'></i>{' '}
+                    {!inCart ? t('AddToCart') : t('Added')}
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
